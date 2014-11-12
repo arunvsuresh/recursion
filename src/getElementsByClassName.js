a334f2f922a43600a5d11d925c5338f010147992
Arun Suresh <arunvsuresh@gmail.com>
2014-11-12 15:26:04 -0800

    Complete getElementsByClassName, stringifyJSON

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,9 +1,40 @@
-// If life was easy, we could just do things the easy way:
-// var getElementsByClassName = function (className) {
-//   return document.getElementsByClassName(className);
-// };
+// // If life was easy, we could just do things the easy way:
+// // var getElementsByClassName = function (className) {
+// //   return document.getElementsByClassName(className);
+// // };
 
-// But instead we're going to implement it from scratch:
+// // But instead we're going to implement it from scratch:
+
 var getElementsByClassName = function(className){
   // your code here
+
+  // body of document (contains entire content)
+  var body = document.body;
+
+  // all the elements under the class name tree
+  var foundElements = [];
+  
+  
+  var findClass = function(element) {
+
+    if (element.classList) {
+    	for (var i = 0; i < element.classList.length; i++) {
+    		if (element.classList[i] === className) {
+    			foundElements.push(element);
+    		}
+    	}
+    	
+  	  for (var i = 0; i < element.childNodes.length; i++) {
+  	  	findClass(element.childNodes[i]);
+  	  }
+
+  	}
+
+  }
+
+  findClass(body);
+  
+  return foundElements;
+
 };
+
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -1,7 +1,62 @@
-// this is what you would do if you liked things to be easy:
-// var stringifyJSON = JSON.stringify;
+// // // this is what you would do if you liked things to be easy:
+// // // var stringifyJSON = JSON.stringify;
 
-// but you don't so you're going to write it from scratch:
+// // // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
   // your code goes here
+  
+  var JSON_string = "{";
+  // BASE CASES
+  if (Array.isArray(obj)) {
+    var resultArr = "[";
+    if (obj.length > 0) {
+      // inspect each element in array
+      for (var i = 0; i < obj.length; i++) {
+        if (obj.indexOf(obj[i]) != obj.length - 1) {
+          resultArr += stringifyJSON(obj[i]) + ",";
+        }
+        else {
+          resultArr += stringifyJSON(obj[i]);
+        }
+      }
+    }
+    return resultArr += "]";
+  }
+
+  else {
+    if (typeof obj === "string") {
+      return "\"" + obj + "\"";
+    }
+    if (typeof obj === "number" || typeof obj === "boolean") {
+      return obj.toString();
+    }
+    
+    if (obj === null) {
+      return "null";
+    }
+    else {
+      
+      for (var key in obj) {
+        if (typeof obj[key] === "function" || typeof obj[key] === undefined) {
+          return JSON_string + "}";
+        }
+        var keys = Object.keys(obj);
+        if (keys.indexOf(key) != keys.length - 1) {
+          JSON_string += stringifyJSON(key) + ":" + stringifyJSON(obj[key]) + ",";
+        }
+        else {
+          JSON_string += stringifyJSON(key) + ":" + stringifyJSON(obj[key])
+        }
+      }
+    }
+    
+  }
+  
+  return JSON_string + "}";
 };
+
+
+
+
+
+
